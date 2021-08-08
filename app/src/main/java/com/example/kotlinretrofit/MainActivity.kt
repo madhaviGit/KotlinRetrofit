4ecbb116277d8e05e925f32e75a4c5afed16be99
MadhaviGullipalli <projects.madhavi@gmail.com>
2021-08-08 12:15:35 +0200

    Third Commit

--- app/src/main/java/com/example/kotlinretrofit/MainActivity.kt
+++ app/src/main/java/com/example/kotlinretrofit/MainActivity.kt
@@ -17,7 +17,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         getMyData()
-       Log.d("Halo","I have added data to Feature1")
+       Log.d("Halo","I have added data to master an created new branch Feature 1 ")
+        Log.d("Halo","data to new branch Feature 1 ")
     }
 
     companion object {
